d87df7f8063aecdcaed91231706ff871352252f0
arzugedik3 <arzu.gedik@trtworld.com>
2024-05-13 15:49:06 +0300

    update

--- CastVideos.js
+++ CastVideos.js
@@ -425,7 +425,7 @@ CastPlayer.prototype.setupRemotePlayer = function () {
     playerTarget.load = function (mediaIndex) {
         console.log('Loading...' + this.mediaContents[mediaIndex]['title']);
         var mediaInfo = new chrome.cast.media.MediaInfo(
-            this.mediaContents[mediaIndex]['sources'][0], 'application/dash+xml');
+            this.mediaContents[mediaIndex]['sources'][0], 'video/mp4');
 
         mediaInfo.metadata = new chrome.cast.media.GenericMediaMetadata();
         mediaInfo.metadata.metadataType = chrome.cast.media.MetadataType.GENERIC;
@@ -936,7 +936,7 @@ CastPlayer.getErrorMessage = function(error) {
 var mediaJSON = { 'categories' : [{ 'name' : 'Movies',
     'videos' : [
         { 'description' : "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
-            'sources' : ['https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd'],
+            'sources' : ['http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'],
             'subtitle' : 'By Blender Foundation',
             'thumb' : 'images/BigBuckBunny.jpg',
             'title' : 'Big Buck Bunny'
